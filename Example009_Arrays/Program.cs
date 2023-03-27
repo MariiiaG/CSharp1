1029794c7dabccb296801f3e085a028d5b936101
Mariia <maria.gadova@gmail.com>
2023-03-27 09:16:52 +0300

    intro to arrays

---
+++ Example009_Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿// int[] array = {11, 21,31, 41, 15, 61, 17, 18, 19}; //intorducing array
+// array[0] = 12; // setting value of array item with index 0 to 12
+// Console.WriteLine(array[0]); // addressing to the value of index 0
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int[] array = {11, 211,31, 41, 15, 61, 17, 18, 19};
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
