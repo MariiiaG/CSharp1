9f46d74bd1f1b4b18093a4f6c5c4f24023825bce
Mariia <maria.gadova@gmail.com>
2023-03-27 13:20:53 +0300

    zadacha1

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,53 @@
+﻿void FillArray(int[] collection) //collection - название аргумента
+{
+    int length = collection.Length; // получть длину массива
+    int index = 0; // начальная позиция индекса
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10); 
+         // заполняем массив рандомными числами от 1 до 10
+        index++;
+    }
+}
+
+void PrintArray(int[] col) //void - метод, кот не возвращает ничего
+                            // на выходе. И не требует return 
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find) // новый метод IndexOf
+// в кач аргументов массив collection и элемент find
+{
+    int count = collection.Length;
+    int index = 0; 
+    int position = 0; // позиция элемента find которую мы ищем
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break; // to only show the first entry
+        }
+        index++;
+    }
+    return position;
+
+}
+int[] array = new int[10]; //create new array 'array' with 10 elements
+
+FillArray(array);
+array[3] = 4; // можно принудительно заполнить некоторые 
+array[7] = 4; // позиции четвёрками
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
---
+++ Sem2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+int num = new Random().Next(100, 1000);
+int a1 = num / 100;
+int a2 = num % 10;
+Console.WriteLine(num);
+Console.WriteLine($"{a1}  {a2}");
