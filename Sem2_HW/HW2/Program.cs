f7091316da19765a8782b74f6ea96afa5a92c255
Mariia <maria.gadova@gmail.com>
2023-03-29 18:02:50 +0300

    HW2

---
+++ Sem2_HW/HW2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100){
+    Console.WriteLine("Третьей цифры нет");
+}
+else{
+    int result = (num % 10);
+    Console.WriteLine($"{result}");
+}
