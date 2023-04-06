5ef070f82e9ff9a713834a51027a3109f0f608a7
Mariia <maria.gadova@gmail.com>
2023-04-06 10:33:55 +0300

    Lec3 examples. Types of methods. Loops

---
+++ Lec3/Ex01/Program.cs
@@ -0,0 +1,55 @@
+﻿// виды методов
+
+// 1. Ничего не принимают, ничего не возвращают (через void)
+//
+// void Method1()  //создаём метод
+// {
+//     Console.WriteLine("Автор...");
+// }
+// Method1();  // вызываем метод
+
+// 2. Что-то принимают, ничего не возвращают (через void)
+//
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2("Текст сообщения");
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i ++;
+//     }
+// }
+// Method21("Текст", 4) // Method21(count : 4, msg : "Текст") <-- именнованные аргументы, если пишем их не по порядку
+
+// 3. Ничего не принимают, что-то возвращают
+//
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3(); // присваиваем полученное значение переменной year
+// Console.WriteLine(year); //выводим значение
+
+// 4. Что-то принимают, что-то возвращают
+//
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty; // создаёт пустую строку, которая будет заполнена при выполнении метода
+        
+//         while (i < count)
+//         {
+//             result = result + text;
+//             i ++;
+//         }
+//         return result;
+// }
+
+// string res = Method4(10, "our text goes in here ");
+// Console.WriteLine(res);
---
+++ Sem4/Program.cs
@@ -0,0 +1,44 @@
+﻿// 26 Напишите программу,
+//  которая принимает на вход число и выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+//
+// int CountNum(int n)
+// {
+//     int count = 0;
+//
+//     while (n > 0)
+//     {
+//         n /= 10;
+//         count++;
+//     }
+//     return count;
+// }
+//
+// Console.Write("Введите число: ");
+// int s = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Количество цифр = {CountNum(s)}");
+ 
+
+//  int MoulNumber (int n) {
+//     int moul = 1;
+//     for (int i = 1; i <= n; i++)
+//         moul = moul * i;
+//     return moul;
+// }
+//
+// Console.WriteLine("Введите целое число: ");
+// int s = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Произведение чисел равно {MoulNumber(s)}");
+
+
+//30 Напишите программу, которая выводит массив из 8 элементов, 
+//заполненный нулями и единицами в случайном порядке.
+
+//[1,0,1,1,0,1,0,0]
+// int a = new Random().Next(2, 7);
+// int b = new Random().Next(2);
+// Console.WriteLine($"a = {a},b = {b}");
+
