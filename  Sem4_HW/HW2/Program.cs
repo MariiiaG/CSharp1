468b9b58e03c11cdbcc9852f393611a1dc869a90
Mariia <maria.gadova@gmail.com>
2023-04-06 12:20:33 +0300

    Sem 4 HW2

---  Sem4_HW/HW2/Program.cs
+++  Sem4_HW/HW2/Program.cs
@@ -3,3 +3,18 @@
 // 82 -> 10
 // 9012 -> 12
 
+int SumDigits (int n)
+{
+    int result = n%10;
+    while (n > 10)
+    {
+        n = n/10;
+        int temp = n%10;
+        result += temp;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {num} равна {SumDigits(num)}");
