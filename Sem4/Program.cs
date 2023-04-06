26c6e5806d03e08537c0ca74f06d459ea9539e7b
Mariia <maria.gadova@gmail.com>
2023-04-06 11:51:06 +0300

    Sem 4 HW1

--- Sem4/Program.cs
+++ Sem4/Program.cs
@@ -1,44 +1,73 @@
-﻿// 26 Напишите программу,
-//  которая принимает на вход число и выдаёт количество цифр в числе.
+﻿// 24. Напишите программу, которая принимает на вход число А
+// и выдаёт сумму чисел от 1 до А
+// а = 5
+// sum = 1 + 2 + 3 + 4 + 5
 
+// int GetSum(int n)
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= n; i ++)
+//         sum += i; // same as sum = sum + 1
+//     return sum;
+// }
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Сумма от 1 до {n} равна {GetSum(n)}");
+
+
+// 26 Напишите программу,
+//  которая принимает на вход число и выдаёт количество цифр в числе.
 // 456 -> 3
 // 78 -> 2
 // 89126 -> 5
-//
+
 // int CountNum(int n)
 // {
 //     int count = 0;
-//
 //     while (n > 0)
 //     {
-//         n /= 10;
+//         n /= 10; // same as n = n / 10
 //         count++;
 //     }
 //     return count;
 // }
-//
 // Console.Write("Введите число: ");
 // int s = int.Parse(Console.ReadLine());
 // Console.WriteLine($"Количество цифр = {CountNum(s)}");
  
 
-//  int MoulNumber (int n) {
-//     int moul = 1;
-//     for (int i = 1; i <= n; i++)
-//         moul = moul * i;
-//     return moul;
+// 28. Напишите программу, которая принимает на вход число N 
+// и выдаёт произведение чисел от 1 до N    
+// 4 --> 24
+// 5 --> 120
+
+//  int MulNumber (int n) {
+//     int mul = 1;
+//     for (int i = 2; i <= n; i++) // start with 2 as multiplying by 1 doesn't change the result
+//         mul *= i;  // same as mul = mul * i
+//     return mul;
 // }
-//
 // Console.WriteLine("Введите целое число: ");
 // int s = int.Parse(Console.ReadLine());
-// Console.WriteLine($"Произведение чисел равно {MoulNumber(s)}");
+// Console.WriteLine($"Произведение чисел равно {MulNumber(s)}");
 
 
 //30 Напишите программу, которая выводит массив из 8 элементов, 
 //заполненный нулями и единицами в случайном порядке.
-
 //[1,0,1,1,0,1,0,0]
-// int a = new Random().Next(2, 7);
-// int b = new Random().Next(2);
+// int a = new Random().Next(2, 7); выдаст числа от 2 до 6
+// int b = new Random().Next(2); выдаст числа от 0 до 1
 // Console.WriteLine($"a = {a},b = {b}");
 
+// int[] GetBinaryArray (int size)
+// {
+//     int[] result = new int[size];
+//     for (int i = 0; i < size; i ++)
+//     {
+//         result[i] = new Random().Next(2); // Next(2) selects numbers from 0-1
+//     }
+//     return result;
+// }
+// int[] array = GetBinaryArray(8);
+// Console.WriteLine($"[{String.Join(", ", array)}]"); // String.Join joins string elements ", " with our array
---
+++  Sem4_HW/HW1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Pow (int a, int b)
+{
+    int result = a;
+    for (int i = 1; i < b; i ++)
+    {
+        result *= a;
+    }
+    return result;
+}
+
+Console.Write("Введите число A : ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число B : ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"{a} в степени {b} = {Pow(a, b)}");
