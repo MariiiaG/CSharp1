8f29f7895fb637967188860df9b1dadd7ef26df5
Mariia <maria.gadova@gmail.com>
2023-04-10 11:48:16 +0300

    Sem 5 HW2

---
+++ Sem5_HW/HW2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GetArray (int size, int MinValue, int MaxValue)
+{
+    int [] result = new int[size];
+    for (int i = 0; i < size; i ++)
+    {
+        result[i] = new Random().Next(MinValue, MaxValue + 1);
+    }
+    return result;
+}
+
+int SumOddElements (int [] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i+=2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+Console.Clear();
+int [] myArray = GetArray(4, 0, 99);
+Console.WriteLine($"[{String.Join(", ", myArray)}] --> {SumOddElements(myArray)}");
