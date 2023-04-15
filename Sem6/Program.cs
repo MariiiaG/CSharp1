9b89c064fe1529c02e6e3d8137b59d0a3280cf83
Mariia <maria.gadova@gmail.com>
2023-04-15 21:17:48 +0300

    Sem 6 problems

---
+++ Sem6/Program.cs
@@ -0,0 +1,151 @@
+﻿// Задача 39. Напишите программу, которая перевернёт массив
+// (последний элемент будет первым, а первый последним)
+
+//  int[] GetArray (int size, int MinValue, int MaxValue)
+// {
+//     int[] result = new int[size];
+//     for (int i = 0; i < size; i ++)
+//     {
+//         result[i] = new Random().Next(MinValue, MaxValue + 1);
+//     }
+//     return result;
+// }
+//
+// void ReverseArray1 (int [] inArray) // changes the current array, as we only use the working array here
+// {
+//     for (int i = 0; i < inArray.Length / 2; i ++)
+//     {
+//         int k = inArray[i];
+//         inArray[i] = inArray[inArray.Length - i - 1];
+//         inArray[inArray.Length - i - 1] = k;
+//     }
+// }
+//
+// int[] ReverseArray2 (int [] inArray) // will return a new reversed array result
+// {
+//     int[] result = new int [inArray.Length]; // creates a new empty array
+//     for (int i = 0; i < inArray.Length; i ++)
+//     {
+//         result[i] = inArray[inArray.Length - 1 - i]; // reverses inArray and saves it as result
+//     }
+//     return result;
+// }
+//
+// Console.Clear();
+//
+// int [] array = GetArray(10, 0, 10);
+// Console.WriteLine(String.Join(" ", array));
+// Console.WriteLine(String.Join(" ", ReverseArray2(array)));
+// Console.WriteLine(String.Join(" ", array));
+// ReverseArray1(array);
+// Console.WriteLine(String.Join(" ", array));
+
+
+// Задача 40: Напишите программу, которая принимает на вход 3 числа и проверяет,
+// может ли существовать треугольник со сторонами такой длины
+// " Теорема о неравенстве треугольника :
+// Каждая сторона треугольника меньше суммы двух других сторон
+
+// Console.Clear();
+// Console.Write("Введите стороны треугольника через запятую : ");
+//
+// string [] sides = Console.ReadLine().Split(',');
+// int a = int.Parse(sides[0]);
+// int b = int.Parse(sides[1]);
+// int c = int.Parse(sides[2]);
+//
+// bool PossiblyTriangle (int a, int b, int c)
+// {
+//     return (((a + b) > c) && ((a + c) > b) && ((b + c) > a));
+// }
+//
+// if (PossiblyTriangle(a, b, c))
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine("No");
+// }
+
+
+// Задача 42 : Напишите программу, которая будет преобразовывать десятичное число в двоичное
+
+// Console.Clear ();
+// int number = int.Parse(Console.ReadLine());
+//
+// string res = DecToNum(number, 16);
+//
+// Console.WriteLine($"{number} --> {res}");
+//
+// string DecToNum(int decNumber, int otherSystem) // универсальный метод перевода из десятичной систему в любую
+// {
+//     string res = ""; // пустая строка для результата
+//     string nums = "0123456789ABCDEF";
+//     while (decNumber > 0)
+//     {
+//         res = nums[decNumber % otherSystem] + res;
+//         decNumber /= otherSystem;
+//     }
+//     return res;
+// }
+
+
+// Задача 44 : Не используя рекурсию, выведите первые N чисел Фибоначи.
+// Первые два числа Фибоначи 0 и 1.
+// Если N 7 --> 0 1 1 2 3 5 8
+
+// int[] Fib(int n)
+// {
+//     int[] mas_f = new int[n];
+//     mas_f[0] = 0;
+//     if (n >= 2)
+//     {
+//         mas_f[1] = 1;
+//     }
+//     else
+//     {
+//         return mas_f;
+//     }
+//     for (int i = 2; i < n; i++)
+//     {
+//         mas_f[i] = mas_f[i - 1] + mas_f[i - 2];
+//     }
+//     return mas_f;
+// }
+//
+// Console.Clear();
+// Console.WriteLine("Enter N : ");
+// int n = int.Parse(Console.ReadLine());
+// Console.WriteLine($"[{String.Join(' ', Fib(n))}]");
+
+
+// Задача 45 : Напишите программу, которая будет создавать копию заданного массива 
+// с помощью поэлементного копирования 
+
+// int[] GetArray (int size, int minValue, int maxValue)
+// {
+//     int[] result = new int[size];
+//     for (int i = 0; i < size; i ++)
+//     {
+//         result[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return result;
+// }
+//
+// int[] CopyArray (int[] inArray)
+// {
+//     int[] res = new int[inArray.Length];
+//     for (int i = 0; i < inArray.Length; i ++)
+//     {
+//         res[i] = inArray[i];
+//     }
+//     return res;
+// }
+//
+// Console.Clear();
+// int[] array = GetArray(10, 0, 10);
+// Console.WriteLine(String.Join(" ", array));
+//
+// int[] newArray = CopyArray(array);
+// Console.WriteLine(String.Join(" ", CopyArray(newArray)));
---
+++ Sem7/Program.cs
@@ -0,0 +1,161 @@
+﻿Clear();
+
+Write("Введите количество строк массива: ");
+int rows=int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns=int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+//int[,] array1 = GetArray(rows, columns, -10, 10);
+//int[,] array2 = GetArray(rows, columns, -100, 100);
+PrintArray(array);
+// mas = [[1, 2, 3],
+//  [4, 5, 6]]
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
+
+// **Задача 48:** Задайте 
+// двумерный массив размера m на n, каждый элемент
+//  в массиве находится по формуле: A[i, j] = i + j. 
+//Выведите полученный массив на экран.
+
+// m = 3, n = 4.
+
+// 0 1 2 3
+
+// 1 2 3 4
+
+// 2 3 4 5
+
+
+
+// **Задача 49:** Задайте двумерный массив.
+//  Найдите элементы, у которых оба индекса чётные,
+//   и замените эти элементы на их квадраты.
+
+// Например, изначально массив выглядел вот так:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 1 2 3 4
+
+// Новый массив будет выглядеть вот так:
+
+// 1 4 7 2
+
+// 5 **81** 2 **9**
+
+// 8 4 2 4
+
+// 1 **4** 3 **16**
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+WriteLine();
+ChangeArray(array);
+PrintArray(array);
+WriteLine();
+PrintArray(ChangeArray(array));
+WriteLine();
+PrintArray(array);
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+int[,] ChangeArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 1 && j % 2 == 1)
+            {
+                array[i, j] *= array[i, j];
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+
+// Задача 51: Задайте двумерный массив. 
+// Найдите сумму элементов, находящихся
+//  на главной диагонали (с индексами (0,0); (1; 1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int GetSumDiag(int[,] array)
+{
+    int length = array.GetLength(0) < array.GetLength(1) ? array.GetLength(0) : array.GetLength(1);
+    // if (array.GetLength(0) < array.GetLength(1))
+    //     length = array.GetLength(0);
+    // else
+    //     length = array.GetLength(1);
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i, i];
+    }
+    return sum;
+}
