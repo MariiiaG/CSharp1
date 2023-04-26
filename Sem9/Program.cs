7c017e5e66080c13301f6ef176ee05311ce2919f
Mariia <maria.gadova@gmail.com>
2023-04-26 16:12:42 +0300

    Sem9 HW3

---
+++ Sem9/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 63 : Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 --> "1, 2, 3, 4, 5"
+// N = 8 --> "1, 2, 3, 4, 5, 6, 7, 8"
+
+using System;
+using static System.Console;
+
+// Clear();
+// Write("Введите число N : ");
+// int N = int.Parse(ReadLine());
+// Num1toN(N, 1);
+
+// void Num1toN (int numN, int i)
+// {
+//     if (i <= numN)
+//     {
+//         Write(i + " ");
+//         i ++;
+//         Num1toN(numN, i);
+//     }
+// }
+
+// Задача 65 : Задайте значение M и N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от М до N.
+// M = 1; N = 5 --> "1, 2, 3, 4, 5"
+// M = 4; N = 8 --> "4, 5, 6, 7, 8"
+
+Clear();
+Write("Введите число M : ");
+int M = int.Parse(ReadLine());
+Write("Введите число N : ");
+int N = int.Parse(ReadLine());
+// NumMtoN(M, N); // for void solution
+Write(NumMtoN(M, N)); // for string solution
+
+// void NumMtoN (int i, int numN)
+// {
+//     if (i <= numN)
+//     {
+//         Write(i + ", ");
+//         i ++;
+//         NumMtoN(i, numN);
+//     }
+// }
+
+string NumMtoN (int numM, int numN)
+{
+    if (numM == numN)
+    {
+        return numM.ToString();
+    }
+    return numM + ", " + NumMtoN(++ numM, numN);
+}
---
+++ Sem9_HW/HW3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите число N : ");
+int N = int.Parse(ReadLine());
+DescRec(N);
+
+void DescRec(int n)
+{
+    if (n < 1) return;  
+    Write(n + " ");     
+    DescRec(n - 1);       
+}
