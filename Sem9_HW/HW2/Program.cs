264e9dffcc7b0bdc71b4a8ba7d91be63ee3f9bfe
Mariia <maria.gadova@gmail.com>
2023-04-26 17:05:56 +0300

    Sem9 HW2

--- Sem9_HW/HW2/Program.cs
+++ Sem9_HW/HW2/Program.cs
@@ -1,7 +1,21 @@
-﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-// m = 2, n = 3 -> A(m,n) = 9
-// m = 3, n = 2 -> A(m,n) = 29
+﻿// Задача 66 : Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
 
 using System;
 using static System.Console;
 
+Clear();
+WriteLine ("Введите число М : ");
+int M = int.Parse(ReadLine());
+WriteLine ("Введите число N : ");
+int N = int.Parse(ReadLine());
+WriteLine ($"Сумма натуральных чисел от {M} до {N} = {SumNumMtoN(M, N)}");
+
+int SumNumMtoN (int num1, int num2)
+{
+    if (num1 == num2)
+        return num1;
+    return num1 + SumNumMtoN (++num1, num2);
+}
