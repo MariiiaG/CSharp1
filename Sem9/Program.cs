35a9d5e1405c2a19ec8b094be5b1b78fb0de5078
Mariia <maria.gadova@gmail.com>
2023-04-26 16:48:28 +0300

    Sem9 HW1

--- Sem9/Program.cs
+++ Sem9/Program.cs
@@ -26,29 +26,69 @@ using static System.Console;
 // M = 1; N = 5 --> "1, 2, 3, 4, 5"
 // M = 4; N = 8 --> "4, 5, 6, 7, 8"
 
-Clear();
-Write("Введите число M : ");
-int M = int.Parse(ReadLine());
-Write("Введите число N : ");
-int N = int.Parse(ReadLine());
-// NumMtoN(M, N); // for void solution
-Write(NumMtoN(M, N)); // for string solution
+// Clear();
+// Write("Введите число M : ");
+// int M = int.Parse(ReadLine());
+// Write("Введите число N : ");
+// int N = int.Parse(ReadLine());
+// // NumMtoN(M, N); // for void solution
+// Write(NumMtoN(M, N)); // for string solution
 
-// void NumMtoN (int i, int numN)
+// // void NumMtoN (int i, int numN)
+// // {
+// //     if (i <= numN)
+// //     {
+// //         Write(i + ", ");
+// //         i ++;
+// //         NumMtoN(i, numN);
+// //     }
+// // }
+
+// string NumMtoN (int numM, int numN)
 // {
-//     if (i <= numN)
+//     if (numM == numN)
 //     {
-//         Write(i + ", ");
-//         i ++;
-//         NumMtoN(i, numN);
+//         return numM.ToString();
 //     }
+//     return numM + ", " + NumMtoN(++ numM, numN);
 // }
 
-string NumMtoN (int numM, int numN)
-{
-    if (numM == numN)
-    {
-        return numM.ToString();
-    }
-    return numM + ", " + NumMtoN(++ numM, numN);
-}
+
+// Задача 67 : Напишите программу, которая будет принимать на вход число 
+// и возвращать сумму его цифр
+// 453 --> 12
+// 45 --> 9
+
+// Clear();
+// Write("Введите число N : ");
+// int N = int.Parse(ReadLine());
+
+// WriteLine($"{SumNumbers(N)}");
+
+// int SumNumbers (int num)
+// {
+//     if (num == 0)
+//         return 0;
+//     return (num % 10 + SumNumbers(num / 10));
+// }
+
+
+// Задача 69 : Напишите программу, которая на вход принимает два числа А и В, 
+// и возводит А в целую степень В с помощью рекурсии
+// A = 3; B = 5 --> 243
+// A = 2; B = 3 --> 8
+
+// Clear();
+// Write("Введите число A : ");
+// int num = int.Parse(ReadLine());
+// Write("Введите число B : ");
+// int pow = int.Parse(ReadLine());
+
+// WriteLine($"Число {num} в степени {pow} = {AtoPowB(num, pow)}");
+
+// int AtoPowB (int num, int pow)
+// {
+//     if (pow == 0)
+//         return 1;
+//     return AtoPowB(num, --pow) * num;
+// }
